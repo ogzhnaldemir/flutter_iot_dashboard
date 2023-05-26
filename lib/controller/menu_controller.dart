c2538a5e3f52fca085d5b6f4239a9ae6a9505b2f
ogzhnaldemir <you@example.com>
2023-05-26 14:11:41 +0300

    Testv1

--- lib/controller/menu_controller.dart
+++ lib/controller/menu_controller.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/controller/btn_add.dart';
 import 'package:flutter_app/model/button_add_model.dart';
-import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 class MainMenuController extends ChangeNotifier {
   ButtonAddModel globalModel = ButtonAddModel();
